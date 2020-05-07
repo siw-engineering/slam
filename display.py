b633efc2eb6b4d9a990ae90e4a3617c55812b405
anandhupvr <anandhupvr@gmail.com>
2020-05-07 22:41:51 +0530

    drawing frustum using opengl pygame

--- display.py
+++ display.py
@@ -4,6 +4,12 @@ import cv2
 import OpenGL.GL as gl
 import pangolin
 
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
 
 '''
 Class used for displaying 2D Data(frames from the queue)
@@ -93,6 +99,69 @@ class Display3D(object):
 	def start(self):
 		self.process.start()
 
+
+class DisplayFrustum(object):
+
+	def __init__(self):
+		
+		print("DisplayFrustum")
+
+	def draw_init(self, cam_loc):
+		rect_1 = (cam_loc[0]*-1, cam_loc[1]*2, cam_loc[2]*2)
+		rect_2 = (cam_loc[0]*-1, cam_loc[1]*2, cam_loc[2]*-2)
+		rect_3 = (cam_loc[0]*-1, cam_loc[1]*-2, cam_loc[2]*-2)
+		rect_4 = (cam_loc[0]*-1, cam_loc[1]*-2, cam_loc[2]*2)
+
+		self.verticies = (
+						rect_1,
+						rect_2,
+						rect_3,
+						rect_4,
+						cam_loc
+				)
+		self.edges = (
+					(0, 1),
+					(0, 3),
+					(1, 2),
+					(2, 3),
+					(3, 0),
+					(0, 4),
+					(1, 4),
+					(2, 4),
+					(3, 4)
+					)
+
+	def Frustum(self):
+		glBegin(GL_LINES)
+		for edge in self.edges:
+			for vertex in edge:
+				glVertex3fv(self.verticies[vertex])
+		glEnd()
+
+	def draw(self, cam_loc):
+		self.draw_init(cam_loc)
+		pygame.init()
+		display = (800,600)
+		pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+		gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+		glTranslatef(0.0,0.0, -5)
+
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					quit()
+
+			glRotatef(1, 3, 1, 1)
+			glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+			self.Frustum()
+			pygame.display.flip()
+			pygame.time.wait(10)
+
+# df = DisplayFrustum()
+# df.draw((-1, 0.5, 0.5))
 
 def draw_matches(fm, t):
 	img = fm._f1.img
