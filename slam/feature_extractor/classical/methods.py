aa3b15b44ef904a31e6f1fa02e058269f225a7f7
anandhupvr <anandhupvr@gmail.com>
2020-07-20 15:11:43 +0530

    adding classical and superpoint feature extractor modules

---
+++ slam/feature_extractor/classical/methods.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+def sift(img):
+	feature = cv2.xfeatures2d.SIFT_create()
+	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
+	scores = 0
+	return {
+		'keypoints': feature_pts,
+		'scores': scores,
+		'descriptors': descriptors,
+	}
+
+
+def orb(img):
+	feature = cv2.ORB_create(nfeatures=1500)
+	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
+	scores = 0
+	return {
+		'keypoints': feature_pts,
+		'scores': scores,
+		'descriptors': descriptors,
+	}
+
+
+def surf(img):
+	feature = cv2.xfeatures2d.SURF_create()
+	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
+	scores = 0
+	return {
+		'keypoints': feature_pts,
+		'scores': scores,
+		'descriptors': descriptors,
+	}
+
+
+def good_features(img):
+	pass
+
+
+def fast_feature(img):
+	feature = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
+	scores = 0
+	return {
+		'keypoints': feature_pts,
+		'scores': scores,
+		'descriptors': descriptors,
+	}
