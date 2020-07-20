fca7c8d912ca1c0d0d0468d364214ff9750abfb3
anandhupvr <anandhupvr@gmail.com>
2020-07-20 16:05:04 +0530

    restructuring feature extractor

--- slam/feature_extractor/classical.py
+++ slam/feature_extractor/classical.py
@@ -2,45 +2,81 @@ import cv2
 import numpy as np
 
 
-def sift(img):
+def sift(img, enable_score=False):
 	feature = cv2.xfeatures2d.SIFT_create()
 	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	scores = 0
 	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
-	return {
-		'keypoints': feature_pts,
-		'descriptors': descriptors,
-	}
+	if enable_score:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
+	else:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
 
 
-def orb(img):
+def orb(img, enable_score=False):
 	feature = cv2.ORB_create(nfeatures=1500)
 	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	scores = 0
 	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
-	return {
-		'keypoints': feature_pts,
-		'descriptors': descriptors,
-	}
+	if enable_score:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
+	else:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
 
 
-def surf(img):
+def surf(img, enable_score=False):
 	feature = cv2.xfeatures2d.SURF_create()
 	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	scores = 0
 	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
-	return {
-		'keypoints': feature_pts,
-		'descriptors': descriptors,
-	}
+	if enable_score:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
+	else:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
 
 
-def good_features(img):
+def good_features(img, enable_score=False):
 	pass
 
 
-def fast_feature(img):
+def fast_feature(img, enable_score=False):
 	feature = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
 	feature_pts, descriptors = feature.detectAndCompute(img, None)
+	scores = 0
 	# feature_pts = np.array([x.pt for x in feature_pts], dtype=np.float32)
-	return {
-		'keypoints': feature_pts,
-		'descriptors': descriptors,
-	}
+	if enable_score:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
+	else:
+		return {
+			'keypoints': feature_pts,
+			'scores': scores,
+			'descriptors': descriptors,
+		}
--- slam/feature_extractor/superpoint/model.py
+++ slam/feature_extractor/superpoint/model.py
@@ -43,53 +43,7 @@
 from pathlib import Path
 import torch
 from torch import nn
-
-def simple_nms(scores, nms_radius: int):
-    """ Fast Non-maximum suppression to remove nearby points """
-    assert(nms_radius >= 0)
-
-    def max_pool(x):
-        return torch.nn.functional.max_pool2d(
-            x, kernel_size=nms_radius*2+1, stride=1, padding=nms_radius)
-
-    zeros = torch.zeros_like(scores)
-    max_mask = scores == max_pool(scores)
-    for _ in range(2):
-        supp_mask = max_pool(max_mask.float()) > 0
-        supp_scores = torch.where(supp_mask, zeros, scores)
-        new_max_mask = supp_scores == max_pool(supp_scores)
-        max_mask = max_mask | (new_max_mask & (~supp_mask))
-    return torch.where(max_mask, scores, zeros)
-
-
-def remove_borders(keypoints, scores, border: int, height: int, width: int):
-    """ Removes keypoints too close to the border """
-    mask_h = (keypoints[:, 0] >= border) & (keypoints[:, 0] < (height - border))
-    mask_w = (keypoints[:, 1] >= border) & (keypoints[:, 1] < (width - border))
-    mask = mask_h & mask_w
-    return keypoints[mask], scores[mask]
-
-
-def top_k_keypoints(keypoints, scores, k: int):
-    if k >= len(keypoints):
-        return keypoints, scores
-    scores, indices = torch.topk(scores, k, dim=0)
-    return keypoints[indices], scores
-
-
-def sample_descriptors(keypoints, descriptors, s: int = 8):
-    """ Interpolate descriptors at keypoint locations """
-    b, c, h, w = descriptors.shape
-    keypoints = keypoints - s / 2 + 0.5
-    keypoints /= torch.tensor([(w*s - s/2 - 0.5), (h*s - s/2 - 0.5)],
-                              ).to(keypoints)[None]
-    keypoints = keypoints*2 - 1  # normalize to (-1, 1)
-    args = {'align_corners': True} if int(torch.__version__[2]) > 2 else {}
-    descriptors = torch.nn.functional.grid_sample(
-        descriptors, keypoints.view(b, 1, -1, 2), mode='bilinear', **args)
-    descriptors = torch.nn.functional.normalize(
-        descriptors.reshape(b, c, -1), p=2, dim=1)
-    return descriptors
+import superpoint.utils as utils
 
 
 class SuperPoint(nn.Module):
@@ -164,7 +118,7 @@ class SuperPoint(nn.Module):
         b, _, h, w = scores.shape
         scores = scores.permute(0, 2, 3, 1).reshape(b, h, w, 8, 8)
         scores = scores.permute(0, 1, 3, 2, 4).reshape(b, h*8, w*8)
-        scores = simple_nms(scores, self.config['nms_radius'])
+        scores = utils.simple_nms(scores, self.config['nms_radius'])
 
         # Extract keypoints
         keypoints = [
@@ -174,13 +128,13 @@ class SuperPoint(nn.Module):
 
         # Discard keypoints near the image borders
         keypoints, scores = list(zip(*[
-            remove_borders(k, s, self.config['remove_borders'], h*8, w*8)
+            utils.remove_borders(k, s, self.config['remove_borders'], h*8, w*8)
             for k, s in zip(keypoints, scores)]))
 
         # Keep the k keypoints with highest score
         if self.config['max_keypoints'] >= 0:
             keypoints, scores = list(zip(*[
-                top_k_keypoints(k, s, self.config['max_keypoints'])
+                utils.top_k_keypoints(k, s, self.config['max_keypoints'])
                 for k, s in zip(keypoints, scores)]))
 
         # Convert (h, w) to (x, y)
@@ -192,7 +146,7 @@ class SuperPoint(nn.Module):
         descriptors = torch.nn.functional.normalize(descriptors, p=2, dim=1)
 
         # Extract descriptors
-        descriptors = [sample_descriptors(k[None], d[None], 8)[0]
+        descriptors = [utils.sample_descriptors(k[None], d[None], 8)[0]
                        for k, d in zip(keypoints, descriptors)]
 
         return {
