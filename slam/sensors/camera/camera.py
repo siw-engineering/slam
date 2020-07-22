4d6fd8db98771af0926c61da028130fa3ba8cd4e
cjpurackal <christeejacobs@gmail.com>
2020-07-22 13:16:11 -0700

    cam updates

--- slam/sensors/camera/camera.py
+++ slam/sensors/camera/camera.py
@@ -3,8 +3,16 @@ import numpy as np
 
 class Camera:
 	_buf = None
-	def __init__(self, F=None, stream=None, l=100):
+	def __init__(self, F=None, Cx=100, Cy=100, stream=None, l=100):
 		self.F = F
+		self.Cx = Cx
+		self.Cy	= Cy
+		self.K =np.array([
+			[F,0,Cx],
+			[0,F,Cy],
+			[0,0,1]
+			]) 
+		self.Kinv = np.linalg.inv(self.K)
 		self.stream = stream
 		self._buf = Buffer(l=l)
 		self.stream.start(self._buf)
@@ -15,12 +23,3 @@ class Camera:
 
 	def has_next(self):
 		return self._buf.has_next()
-
-	@property
-	def K(self):
-		return self._K
-
-	@property
-	def Kinv(self):
-		return self._Kinv
-	
