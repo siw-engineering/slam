deee36f687c31dc5a197771979276073cbf61df8
cjpurackal <christeejacobs@gmail.com>
2020-07-24 05:01:21 -0700

    rem for __init__

--- examples/ros_inputs_test.py
+++ examples/ros_inputs_test.py
@@ -8,15 +8,20 @@ from slam.sensors.camera.inputs import RosTopicStream
 from slam.visualizer.display import Display2D
 
 rgb_cam = Camera(cam_type="rgb8",F=100, stream=RosTopicStream("/ROBOTIKA_X2/image_raw"))
-display = Display2D("display")
-display.start()
+rbg_display = Display2D("display")
+depth_display = Display2D("display")
+
+rbg_display.start()
+depth_display.start()
+
 depth_cam = Camera(cam_type="32FC1",F=100, stream=RosTopicStream("/ROBOTIKA_X2/front/depth"))
 
 while 1:
 	img = rgb_cam.read()
 	dimg = depth_cam.read()
 
-	display.q.put([dimg])
+	rbg_display.q.put([img])
+	depth_display.q.put([dimg])
 
 print ("out")
 
