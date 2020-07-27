d32e8fd7099d0e31cfe3ff00e113b23ca5b8fa90
cjpurackal <christeejacobs@gmail.com>
2020-07-27 12:25:37 -0700

    minor

--- examples/sparse_odom.py
+++ examples/sparse_odom.py
@@ -5,23 +5,26 @@ sys.path.append("../")
 import cv2
 import numpy as np
 from slam.sensors.camera import Camera
-from slam.sensors.camera.inputs import VideoStream
+from slam.sensors.camera.inputs import VideoStream, RosTopicStream
 from slam.visualizer.display import Display2D, DisplayCam
 from slam.feature_extractor.classical import orb_features
 from slam.feature_matcher.classical import FrameMatcher
 import slam.pose_recovery as pose
+import pdb
 
-rgb_cam = Camera(cam_type="rgb8",F=100, stream=VideoStream("path/to/video.mp4"))
+# rgb_cam = Camera(cam_type="rgb8",F=100, stream=VideoStream("/home/developer/dataset/car.mp4"))
+rgb_cam = Camera(cam_type="rgb8",F=100, stream=RosTopicStream("/ROBOTIKA_X2/image_raw"))
+# depth_cam = Camera(cam_type="32FC1",F=100, stream=RosTopicStream("/ROBOTIKA_X2/front/depth"))
+
 rbg_display = Display2D("display")
 depth_display = Display2D("display")
 cam_display = DisplayCam("cam")
 
 cam_display.start()
-rbg_display.start()
-depth_display.start()
+# rbg_display.start()
+# depth_display.start()
 
 
-# depth_cam = Camera(cam_type="32FC1",F=100, stream=RosTopicStream("/ROBOTIKA_X2/front/depth"))
 rgb1 = rgb_cam.read()
 
 feat = FrameMatcher("bf")
@@ -32,11 +35,12 @@ while 1:
 	f1 = orb_features(rgb1)
 	f2 = orb_features(rgb2)
 	pts1, pts2 = feat.match(f1, f2)
+	if len(pts1) == 0 or len(pts2) == 0:
+		continue
+	# pdb.set_trace()
 	Rt = pose.by_ransac(pts1, pts2, rgb_cam.K)
 	rgb1 = rgb2
 	# cam_pos = cam_pos + np.subtract(Rt[:3,3], cam_pos)/rgb_cam.sf
 	cam_display.q.put([Rt])
-	# depth_display.q.put([dimg])
 
-print ("out")
 
