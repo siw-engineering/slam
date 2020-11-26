4d924c8961dc80208f4ec43d86ef71eb9b1a4c16
cjpurackal <christeejacobs@gmail.com>
2020-11-26 06:15:37 -0800

    further testing

--- src/main_gpe.cpp
+++ src/main_gpe.cpp
@@ -18,14 +18,15 @@ int main(int argc, char *argv[])
 	cv::Mat dimg, img;
 	int width = 640/2;
 	int height = 480/2;
-	std::shared_ptr<Shader> warper_program;
+	std::shared_ptr<Shader> warper_program, test_program;
 	GSLAM::CameraPinhole cam(320,240,277,277,160,120);
-	GPUTexture* rgbd_frame;
+	// GPUTexture *rgb, *depth;
 	
 	depthsub  = new DepthSubscriber("/X1/front/depth", nh);
 	rgbsub = new RGBSubscriber("/X1/front/image_raw", nh);
 
-	rgbd_frame = new GPUTexture(width, height, GL_LUMINANCE32F_ARB, GL_LUMINANCE, GL_FLOAT);
+	// rgb = new GPUTexture(width, height, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, true, true);
+	// depth = new GPUTexture(width, height, GL_LUMINANCE32F_ARB, GL_LUMINANCE, GL_FLOAT);
 
 
 	/* Display code */
@@ -39,48 +40,81 @@ int main(int argc, char *argv[])
 	.SetBounds(0,1.0f,0,1.0f,-640/480.0)
 	.SetHandler(new pangolin::Handler3D(s_cam)
 	);
-	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST	);
 	glDepthMask(GL_TRUE);
 	glDepthFunc(GL_ALWAYS);
-	warper_program = std::shared_ptr<Shader>(loadProgramFromFile("test.vert", "test.frag"));
-  	
-  	GLuint vbo, vao;
+	test_program = std::shared_ptr<Shader>(loadProgramFromFile("test.vert", "test.frag"));
+	warper_program = std::shared_ptr<Shader>(loadProgramFromFile("warper.vert"));
+  	GLuint vbo, ebo;
     float vertices[] = {
     // positions          // colors           // texture coords
-     -1,  -1, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     -1,  1, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-     1,   1, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-     1,  -1, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+      1,  1, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+      1,  -1, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+     -1,   -1, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+     -1,  1, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 };
-
+	int indices[] = {
+		0,1,3,
+		1,2,3,
+	};
 
   	glGenBuffers(1, &vbo);
   	glBindBuffer(GL_ARRAY_BUFFER, vbo);
   	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  	glGenVertexArrays(1, &vao);
-  	glBindVertexArray(vao);
-  	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), 0);
+  	
+  	// glGenBuffers(1, &ebo);
+  	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+
+  	// glGenVertexArrays(1, &vao);
+  	// glBindVertexArray(vao);
   	glEnableVertexAttribArray(0);
-  	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+  	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), 0);
+  	
+  	// glGenVertexArrays(1, &cao);
+  	// glBindVertexArray(cao);
   	glEnableVertexAttribArray(1);
-  	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)) );
+  	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+  	
+  	// glGenVertexArrays(1, &tao);
+  	// glBindVertexArray(tao);
   	glEnableVertexAttribArray(2);
+  	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)) );
 
-
-	GLuint tid;
+	// glBindVertexArray(0);
+ //    glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	GLuint rgb_tid, depth_tid;
 	void* data;
 	data = NULL;
-	glGenTextures(1,&tid);
-	glBindTexture(GL_TEXTURE_2D, tid);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+	glGenTextures(1,&rgb_tid);
+	glBindTexture(GL_TEXTURE_2D, rgb_tid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	CheckGlDieOnError();
+
+	glGenTextures(1,&depth_tid);
+	glBindTexture(GL_TEXTURE_2D, depth_tid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE32F_ARB, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, 0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	CheckGlDieOnError();
+
+
+	GLuint fid;
+	glGenTransformFeedbacks(1, &fid);
   	// pangolin::GlTexture* imageTexture(width,height,GL_RGB,false,0,GL_RGB,GL_UNSIGNED_BYTE);
 	while (ros::ok())
 	{
+
 		img  = rgbsub->read();
 		dimg = depthsub->read();
 		if (dimg.empty() || img.empty())
@@ -90,42 +124,44 @@ int main(int argc, char *argv[])
 		}
 		d_cam.Activate(s_cam);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		warper_program->Bind();
 		/*texture testing*/
 
-		glBindTexture(GL_TEXTURE_2D, tid);
-		glTexSubImage2D(GL_TEXTURE_2D,0,0,0,width,height,GL_RGB,GL_UNSIGNED_BYTE,(void*)img.data);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, rgb_tid);
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_BGR,GL_UNSIGNED_BYTE,(void*)img.data);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depth_tid);
+		glTexImage2D(GL_TEXTURE_2D,0,GL_LUMINANCE32F_ARB, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, (void*)dimg.data);
+		
+
+		test_program->Bind();
 
 	    glMatrixMode(GL_PROJECTION);
 	    glLoadIdentity();
 	    glMatrixMode(GL_MODELVIEW);
 	    glLoadIdentity();
 
-	    // GLfloat sq_vert[] = { -1,-1,  1,-1,  1, 1,  -1, 1 };
-	    // glVertexPointer(2, GL_FLOAT, 0, sq_vert);
-	    // glEnableClientState(GL_VERTEX_ARRAY);
 
-	    // GLfloat sq_tex[]  = { 0,0,  1,0,  1,1,  0,1  };
-	    // glTexCoordPointer(2, GL_FLOAT, 0, sq_tex);
-	    // glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+		test_program->Unbind();
 
-	    glEnable(GL_TEXTURE_2D);
-		glBindTexture(GL_TEXTURE_2D, tid);
-	    glDrawArrays(GL_TRIANGLES, 0, 4);
+
+		
+		// glDrawArrays(GL_POINTS, 0, width*height);  // GPU-PASS
+
+
 	    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-
-		warper_program->Unbind();
-
-	    glDisableClientState(GL_VERTEX_ARRAY);
-	    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-	    glDisable(GL_TEXTURE_2D);
+	    // glDisableClientState(GL_VERTEX_ARRAY);
+	    // glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	    // glDisable(GL_TEXTURE_2D);
 
 		/*pangolin texture testing
 
        	imageTexture.Upload(img.data,GL_RGB,GL_UNSIGNED_BYTE);
     	imageTexture.RenderToViewport();*/
-
+		// glBindTexture(GL_TEXTURE_2D, 0);
 	    pangolin::FinishFrame();
 		/*debug code		
 		double minVal; 
