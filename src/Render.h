1fc0c6b8201a33f302fc2f979b78c7b1ba240549
anandhupvr <anandhupvr@gmail.com>
2021-04-28 00:28:38 -0700

    texture fix :

--- src/Render.h
+++ src/Render.h
@@ -64,6 +64,33 @@ public:
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
+	void Texture()
+	{
+		glGenTextures(1, &m_RenderingID);
+	}
+
+	void TexGen(unsigned int width, unsigned int height, unsigned char* data)
+	{
+
+		glBindTexture(GL_TEXTURE_2D, m_RenderingID); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   // set texture wrapping to GL_REPEAT (default wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+	}
+
+	void textureBind()
+	{
+		glBindTexture(GL_TEXTURE_2D, m_RenderingID);
+	}
+
 	pangolin::OpenGlMatrix GetMvp()
 	{
 
