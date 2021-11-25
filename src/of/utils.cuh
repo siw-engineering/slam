5052ce2596ddcf4f58a3cacbe4cf7d3d3b90ef1e
cjpurackal <christeejacobs@gmail.com>
2021-11-25 17:52:19 +0000

    error fix

--- src/of/utils.cuh
+++ src/of/utils.cuh
@@ -2,4 +2,4 @@
 #include "../cuda/types.cuh"
 
 
-void computeCameraVelOF(DeviceArray<float>2D& angle_mat, DeviceArray<float>2D& mag_mat, DeviceArray<float>2D& dimg, const float3 ang_vel, DeviceArray<float>2D& cam_vel, float fx, int cols, int rows);
+void computeCameraVelOF(DeviceArray2D<float>& angle_mat, DeviceArray2D<float>& mag_mat, DeviceArray2D<float>& dimg, const float3 ang_vel, DeviceArray2D<float>& cam_vel, float fx, int cols, int rows);
